98aa9c4f157eb831b6a30fdcc59bf69e2a6b5bbf
Siriboon Chaisawat <siriboon@iapp.co.th>
2019-12-20 16:14:22 +0700

    aff footer and scrolling detection

---
+++ src/components/footer.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.styles.scss'
+
+class Footer extends React.Component {
+  render(){
+    // const {numAttendee,numVotedVoter} = this.props;
+    return(
+      <div className='footer-style'>
+        <h2>This is footer</h2>
+      </div>
+    )
+  }
+}
+export default Footer;
--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -3,71 +3,103 @@ import '../logo.svg'
 import StickyHeader from 'react-sticky-header'
 import 'react-sticky-header/styles.css'
 import './header.styles.scss'
+import Footer from './footer.component.jsx'
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {scrollStatus:'scroll stopped'};
+        this._timeout = null;
+        this.handleScroll = this.handleScroll.bind(this);
     }
+
+    componentDidMount(){
+      window.addEventListener('scroll', (e)=>this.handleScroll(e.target));
+    }
+
+
+    handleScroll(event) {
+      console.log('Im here');
+     if(this._timeout){ //if there is already a timeout in process cancel it
+      clearTimeout(this._timeout);
+     }
+     this._timeout = setTimeout(() => {
+       this._timeout = null;
+       this.setState({
+         scrollStatus:'scroll stopped'
+       });
+     },1000);
+     if(this.state.scrollStatus !== 'scrolling') {
+       this.setState({
+         scrollStatus:'scrolling'
+       });
+     }
+
+  }
 
     render() {
         return (
-            <StickyHeader header={
-                <div className='header-layer'>
-                    <div className='logo-information'>
-                        <div><img alt='logo'/></div>
-                        <div className='logo-title'>
-                            <span className='e-voting'>e-Voting</span>
-                            <span className='description-text'>distributed electronic voting platform</span>
-                        </div>
-                    </div>
-                <div className='menu'>
-                </div>
-            </div>
-            }>
-        
+          <div>
+          <div className='header-layer' style={{position:'fixed'}}>
+              <div className='logo-information'>
+                  <div><img alt='logo'/></div>
+                  <div className='logo-title'>
+                      <span className='e-voting'>e-Voting</span>
+                      <span className='description-text'>distributed electronic voting platform</span>
+                  </div>
+              </div>
+          <div className='menu'>
+          </div>
+      </div>
+
             <div className='body'>
                 <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
-                        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
+                        <h1>{this.state.scrollStatus}</h1>
                         <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
             </div>
-            </StickyHeader>
+
+            {this.state.scrollStatus=='scroll stopped'&&(
+              <Footer/>
+            )}
+
+            </div>
         );
     }
 }
